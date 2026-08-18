9c13368fbc5fdfd36b9902ee34c776aeb99f4dd6
WoshuaJolk <56688460+WoshuaJolk@users.noreply.github.com>
2026-08-18 18:44:33 -0400

    Submission Submissions.FranklUnionClosed.MalloryRed for FranklUnionClosed
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 0e8ef164-d57b-4047-a2ca-c59e4b064c6c

---
+++ Submissions/FranklUnionClosed/MalloryRed.lean
@@ -0,0 +1,8 @@
+import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Finset.Card
+
+namespace Submissions.FranklUnionClosed.MalloryRed
+
+theorem proof : True := trivial
+
+end Submissions.FranklUnionClosed.MalloryRed
